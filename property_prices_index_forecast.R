5bd68e82e87a0fb11f20a4f504868c2316e6b8a3
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-10-20 18:54:07 +0200

    Added data preprocessing for housing loans index in order to prepare it for transformation from monthly to yearly data

--- property_prices_index_forecast.R
+++ property_prices_index_forecast.R
@@ -8,25 +8,29 @@ gdp_per_capita = read.csv("gdp_per_capita.csv", sep=",", nrows = 1,check.names =
 house_price_index = read.csv("house_price_index.csv", sep=";",check.names = FALSE)
 housing_loans_interest_rates = read.csv("housing_loans_interest_rates.csv", sep=";",check.names = FALSE)
 
+
+housing_loans_interest_rates <- head(housing_loans_interest_rates,1)
+names(housing_loans_interest_rates) <- as.numeric(substr(colnames(housing_loans_interest_rates[-1]),4,5))
+names(housing_loans_interest_rates)[1] <- "Loan type"
+names(housing_loans_interest_rates)[ncol(housing_loans_interest_rates)] <- "20"
+
 # Get the average of monthly interest rates on yearly basis
 
 library(dplyr)
 
-
-
 year = unique(as.numeric(substr(colnames(housing_loans_interest_rates[-1]),4,5)))
-mutate(housing_loans_interest_rates, year = year)
-
-
-housing_loans_interest_rates[year] <- NA
-
-cbind(housing_loans_interest_rates, setNames( lapply(year, function(x) x=NA), year) )
 
 #next - group by distinct year values and get the average of the interest rates
 
-group_by(distinct(as.data.frame(as.numeric(substr(colnames(housing_loans_interest_rates[-1]),4,5))))) %>%
+group_by(year) %>%
 summarise(mean_int_rates = mean(as.numeric(substr(colnames(housing_loans_interest_rates[-1]),4,5))),n = n())
 
+housing_loans_interest_rates <- lapply(housing_loans_interest_rates, as.character)
+housing_loans_interest_rates <- gsub(",",".",housing_loans_interest_rates)
+housing_loans_interest_rates <- lapply(housing_loans_interest_rates, as.double)
+
+
+colMeans(housing_loans_interest_rates[2:12])
 
 
 # Binding datasets by rows
